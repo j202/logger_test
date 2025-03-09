1b6fdac89764baa2fddb623809f330e77e0f1c4b
j202 <j202@users.noreply.github.com>
2025-03-09 17:31:16 +0000

    Update syslog formatter for RFC 5424 compliance
    Add BOM (byte order mark) for RFC 5424 message compliance.
    See https://docs.python.org/3/howto/logging-cookbook.html#inserting-a-bom-into-messages-sent-to-a-sysloghandler

--- src/app/log_setup.py
+++ src/app/log_setup.py
@@ -7,6 +7,18 @@ import sys
 log = logging.getLogger("App")
 
 
+class SyslogFormatter(logging.Formatter):
+    """Create an RFC 5424 compliant syslog formatter.
+
+    The logging syslog formatter as-is is non-compliant.
+    See the URL below for details.
+    https://docs.python.org/3/howto/logging-cookbook.html#inserting-a-bom-into-messages-sent-to-a-sysloghandler
+    """
+
+    def format(self, record):
+        return "\ufeff" + super().format(record)
+
+
 def setup_logging(log_path: Path):
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s %(module)s %(levelname)s %(message)s")
@@ -25,7 +37,7 @@ def setup_logging(log_path: Path):
     sys_handler = logging.handlers.SysLogHandler(
         address="/dev/log", facility=logging.handlers.SysLogHandler.LOG_LOCAL1
     )
-    syslog_formatter = logging.Formatter("%(name)s %(module)s %(levelname)s %(message)s")
+    syslog_formatter = SyslogFormatter("%(name)s %(module)s %(levelname)s %(message)s")
     sys_handler.setFormatter(syslog_formatter)
     sys_handler.setLevel(logging.WARNING)
 
